6ac1001e7bdf4cb79b07dc8dc88d14c7df08c5d2
tonymmm1 <tony@mail.magnatox.com>
2020-03-16 17:49:49 -0500

    release-0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-Release: 0.0.0 
-py-update-script
+##Release: 0.1.0
+##py-update-script
+
+###Overview
+
+This is a simple python script to update linux machines over ssh. It is configurable using a TOML file. 
+
+###Instructions
+
+1. Make sure python3 is installed
+2. Look at hosts.example.toml and copy it to hosts.toml
+3. Configure hosts.toml
+4. Make sure host that is running script has publickey permissions to remote host with defined user
+5. Make sure remote user has sudo permissions for running updates according to distro package manager
+```
+sudo visudo
+```
+```
+root ALL=(ALL) ALL
+#insert after root line and make sure to uncomment correct distro
+#(user) ALL=(ALL) NOPASSWD: /usr/bin/pacman			#Arch
+#(user) ALL=(ALL) NOPASSWD: /usr/bin/pacman,/usr/bin/yay	#Arch with yay package manager
+#(user) ALL=(ALL) NOPASSWD: /usr/bin/yum,/bin/rpm		#CentOS
+#(user) ALL=(ALL) NOPASSWD: /usr/bin/apt,/usr/bin/apt-get	#Debian
+#(user) ALL=(ALL) NOPASSWD: /usr/bin/dnf,/bin/rpm		#Fedora
+```
+6. chmod +x 
+7. (Optional)Automate using cron
+
---
+++ py-update-script.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env  python3
+
+import subprocess
+import toml
+
+debug = 0
+
+host_file = "hosts.toml"
+
+def file_host_read(host_file,debug):
+    host_config = toml.load(host_file)
+    if (debug == 1):
+        print (host_config, "\n")
+    for hosts in host_config.items():
+        if (debug == 1):
+            print ("\n",hosts,"\n")
+        else:
+            print ("\n", hosts[0],"\n")
+        ip_address= hosts[1]["ip_address"]
+        os= hosts[1]["os"]
+        user= hosts[1]["user"]
+        docker= hosts[1]["docker"]
+        port= hosts[1]["port"]
+        commands(ip_address,port,os,user,docker)
+def commands(ip_address, port, os, user, docker):
+    if (os == "arch"):
+        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' "yes | sudo pacman -Syyu && yes | sudo pacman -c"', shell=True)
+    if (os == "arch1"):
+        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' "yes | sudo yay -Syyu && yes | sudo yay -c"', shell=True) 
+    elif (os == "centos"):
+        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' "sudo yum update -y"', shell=True)
+    elif (os == "debian"):
+        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' " sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y"', shell=True)
+    elif (os == "fedora"):
+        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' " sudo dnf update -y"', shell=True)
+
+file_host_read(host_file,debug)
+print ("\nUpdate script complete")
