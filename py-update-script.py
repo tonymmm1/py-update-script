f130fb9295a13ade1139a25b99a24c1847d2a9a8
tonymmm1 <tony@mail.magnatox.com>
2020-05-30 16:39:19 -0500

    updated py-update-script.py

--- py-update-script.py
+++ py-update-script.py
@@ -20,18 +20,19 @@ def file_host_read(host_file,debug):
         os= hosts[1]["os"]
         user= hosts[1]["user"]
         port= hosts[1]["port"]
+#        docker= hosts[1]["docker"]
         commands(ip_address,port,os,user)
 def commands(ip_address, port, os, user):
     if (os == "arch"):
         subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' "yes | sudo pacman -Syyu && yes | sudo pacman -c"', shell=True)
-    if (os == "arch1"):
+    elif (os == "arch1"):
         subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' "yes | sudo yay -Syyu && yes | sudo yay -c"', shell=True) 
     elif (os == "centos"):
         subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' "sudo yum update -y"', shell=True)
     elif (os == "debian"):
-        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' " sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y"', shell=True)
+        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' "sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y"', shell=True)
     elif (os == "fedora"):
-        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' " sudo dnf update -y"', shell=True)
+        subprocess.run("ssh -t -p" + port + " " + user + "@" + ip_address + ' "sudo dnf update -y"', shell=True)
 
 file_host_read(host_file,debug)
 print ("\nUpdate script complete")
